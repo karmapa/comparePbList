6660972d3e7e19f888682049b97c5e194e6c0e47
zhi-xuan <rickie120243@gmail.com>
2016-07-06 15:05:23 +0800

    fix test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var comparePbList = require('./comparePbList.js');
 var dg001 = fs.readFileSync('./input/degekangyur001.txt', 'utf8');
 
-var list = comparePbList(dg001, 'DG');
+var list = comparePbList(dg001);
 console.log(list);
