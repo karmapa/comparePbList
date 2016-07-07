db4938a54ac66dbf972eddd79d39d13a80e46b23
kmsheng <kmsh3ng@gmail.com>
2016-07-07 11:26:30 +0800

    fix typo

--- test2.js
+++ test2.js
@@ -4,7 +4,7 @@ var isPbId = require('./isPbId');
 
 var dg001Text = fs.readFileSync('./input/degekangyur001.txt', 'utf8');
 
-// 分別取得 pb tag 與 jb tag 的 pos data
+// 分別取得 pb tag 與 jp tag 的 pos data
 var pbRows = parseXml(dg001Text, 'pb.id');
 var jpRows = parseXml(dg001Text, 'jp.id');
 
