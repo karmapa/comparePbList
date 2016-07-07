825321b2c3003ccc41dc46ba09878058b7aa62f2
kmsheng <kmsh3ng@gmail.com>
2016-07-07 11:25:39 +0800

    new file test2.js

---
+++ test2.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var parseXml = require('./parseXml');
+var isPbId = require('./isPbId');
+
+var dg001Text = fs.readFileSync('./input/degekangyur001.txt', 'utf8');
+
+// 分別取得 pb tag 與 jb tag 的 pos data
+var pbRows = parseXml(dg001Text, 'pb.id');
+var jpRows = parseXml(dg001Text, 'jp.id');
+
+console.log('pbRows', pbRows);
+console.log('jpRows', jpRows);
